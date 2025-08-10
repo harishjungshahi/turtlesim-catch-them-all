546b20582e418c880310a13b0df1960c80175646
harishjungshahi <shahiharishjung@gmail.com>
2025-08-10 22:44:07 +0545

    Initial commit of ROS 2 workspace

---
+++ build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/junge/ros2_ws/install/my_robot_interfaces/share/my_robot_interfaces/package.sh"
+. "/home/junge/ros2_ws/install/my_py_pkg/share/my_py_pkg/package.sh"
+. "/home/junge/ros2_ws/install/turtlesim_catch_them_all/share/turtlesim_catch_them_all/package.sh"
